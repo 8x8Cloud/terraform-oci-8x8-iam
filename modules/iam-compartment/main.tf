9fa80c6cf3b082d4235809e65b903a2fe72bb32b
Vlad Kuusk <vlad.kuusk@8x8.com>
2020-06-06 21:42:22 -0700

    - remove interpolation to use HCL 2 syntax

--- modules/iam-compartment/main.tf
+++ modules/iam-compartment/main.tf
@@ -5,22 +5,22 @@
 ########################
 
 resource "oci_identity_compartment" "this" {
-  count          = "${var.compartment_create ? 1 : 0}"
-  compartment_id = "${var.target_compartment_ocid}"
-  name           = "${var.compartment_name}"
-  description    = "${var.compartment_description}"
+  count          = var.compartment_create ? 1 : 0
+  compartment_id = var.target_compartment_ocid
+  name           = var.compartment_name
+  description    = var.compartment_description
 }
 
 data "oci_identity_compartments" "this" {
-  count          = "${var.compartment_create ? 0 : 1}"
-  compartment_id = "${var.target_compartment_ocid}"
+  count          = var.compartment_create ? 0 : 1
+  compartment_id = var.target_compartment_ocid
 
   filter {
     name   = "name"
-    values = ["${var.compartment_name}"]
+    values = [var.compartment_name]
   }
 }
 
 locals {
-  compartment_ids = "${concat(flatten(data.oci_identity_compartments.this.*.compartments), list(map("id", "")))}"
+  compartment_ids = concat(flatten(data.oci_identity_compartments.this.*.compartments), list(map("id", "")))
 }
